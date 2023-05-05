42e930adabcbe531ece69da31b2b8157568303bc
dbg1699 <dbenitezgonzalez05@gmail.com>
2023-05-05 07:09:59 -0500

    Proyecto generador de memes con funcionalidades de javascript, maquetación de html y estilos definidos de css

---
+++ README.md
@@ -0,0 +1,22 @@
+## GENERADOR DE MEMES.
+### TRABAJO PRACTICO REALIZADO CON HTML5, CSS3 Y JAVASCRIPT
+
+* La página está diseñada para que puedas pegar el link de una imagen que mas te guste desde la web y editarla desde acá.
+* Tiene un diseño que se adapta a diferentes dispositivos.
+* Con el boton **CAMBIAR TEMA** se puede cambiar al tema claro si lo deseas.
+* Podes guardar la imagen editada con el botón **DESCARGAR MEME**.
+* Dale un vistazo desde acá y generá tu propio meme: 
+[Genedaror de meme]
+
+funciones para realizar:
+
+1. Agregar icono en la pestaña del navegador
+
+2. Función que "cambie tema" de oscuro a claro
+
+3. Al darle clic en el boton imagen que me mande a la opción imagen y que pueda agregar una url de imagen y me lo muestre en el contendeor general principal, en este caso que la imagen se pueda observar en la caja-imagen
+
+4. en el boton texto me deje editar el texto superior e inferior y me lo muestre en el meme
+
+5. funcionamiento del boton descargar
+6. al momento de descargar que me muestre el archivo de descarga con el nombre "mi-meme.png" (ya sea que se descargue en png o jpg)
---
+++ app.js
@@ -0,0 +1,196 @@
+// ********************************************
+//      MODO CLARO | MODO OSCURO        
+// ********************************************
+
+
+
+//*****************************************************************
+//                  BOTON DESCARGA MEME 
+// *****************************************************************
+
+
+// ********************************************************
+// CAMBIO DE COLUMNA IMAGEN A COLUMNA TEXTO CON BOTONES NAV | BOTON CIERRE RESPONSIVE
+// ********************************************************
+const btnImg = document.getElementById('button-text-img');
+const btnText = document.getElementById('button-text-text');
+const panelImagen = document.getElementById('panel-imagen');
+const panelTexto = document.getElementById('panel-texto');
+const btnPanelCierre = document.getElementById('panel-close-button');
+
+btnImg.addEventListener('click', () => {
+    panelImagen.style.display = "block";
+    panelTexto.style.display = "none";
+})
+
+btnText.addEventListener('click',() => {
+    panelTexto.style.display = "block";
+    panelImagen.style.display = "none";
+})
+
+btnPanelCierre.addEventListener('click', () =>{
+    panelTexto.style.display = "none";
+    panelImagen.style.display = "none";
+})
+
+// **********************************
+//         URL INPUT- IMAGEN
+// **********************************
+
+
+// ***************************
+// COLOR DE FONDO DE IMAGEN //CAMBIOS SELECT IMAGEN | MEZCLA DE FONDO
+// **************************
+const contenedora = document.getElementById('caja-meme');
+const fondoColorImg = document.getElementById('incolor');
+const colorValor = document.getElementById('imagen-color');
+const mezcla = document.getElementById('mezcla');
+
+fondoColorImg.addEventListener('input', (event) =>{
+    const color = event.target.value;
+    contenedora.style.background = color;
+    colorValor.innerHTML = `${color}`;
+})
+
+const fondoColor = () => {
+    imagen.style.backgroundColor = fondoColorImg.value;
+}
+
+const actualizarTipoMezcla = () => {
+    imagen.style.backgroundBlendMode = mezcla.value;
+};
+
+fondoColorImg.addEventListener('change', fondoColor);
+mezcla.addEventListener('change', actualizarTipoMezcla);
+
+// *******************************************************
+//+++++PANEL DE FILTROS EN LA IMAGEN////CAJA MEME
+// *******************************************************
+
+/***************BOTON RESTABLECER FILTROS**************/
+
+
+
+// *******************************************************
+//      INPUT TEXTO CAJA MEME | CHECKBOX CAJA MEME////
+// *******************************************************
+
+
+// *******************************************************************
+//      CAMBIO TIPO DE FUENTE
+// *******************************************************************
+
+
+// *******************************************************************
+//      TAMAÑO DE FUENTE
+// *******************************************************************
+const textSize = document.getElementById('text-size-input');
+
+textSize.addEventListener('input', (event) => {
+    const NumberFont = event.target.value;
+    textoTop.style.fontSize = `${NumberFont}px`;
+    textoInf.style.fontSize = `${NumberFont}px`;
+})
+
+// ******************************************************************
+//      ALINEACION DE TEXTO 
+// *****************************************************************
+const btnAlignLeft = document.getElementById('btn-text-left-align');
+const btnAlignCenter = document.getElementById('btn-text-center-align');
+const btnAlignRight = document.getElementById('btn-text-right-align');
+
+btnAlignLeft.addEventListener('click', () => {
+    textoTop.style.textAlign = "left";
+    textoInf.style.textAlign = "left";
+});
+btnAlignCenter.addEventListener('click', () => {
+    textoTop.style.textAlign = "center";
+    textoInf.style.textAlign = "center";
+});
+btnAlignRight.addEventListener('click', () => {
+    textoTop.style.textAlign = "right";
+    textoInf.style.textAlign = "right";
+});
+
+// **************************************************
+//      COLOR DE TEXTO Y FONDO | FONDO TRANSPARENTE
+// *************************************************
+const colorTexto = document.getElementById('incolor2');
+const colorFondoTexto = document.getElementById('incolor3');
+const textoColorValor = document.getElementById('texto-color');
+const fondoColorValor = document.getElementById('fondo-color');
+
+const check3 = document.getElementById('sin-fondo');
+
+colorTexto.addEventListener('input', (event) => {
+    const colorT = event.target.value;
+    textoTop.style.color = `${colorT}`;
+    textoInf.style.color = `${colorT}`;
+    textoColorValor.innerHTML = `${colorT}`;
+})
+colorFondoTexto.addEventListener('input', (event) => {
+    const colorF = event.target.value;
+    textoTop.style.background = `${colorF}`;
+    textoInf.style.background = `${colorF}`;
+    fondoColorValor.innerHTML = `${colorF}`;
+})
+
+check3.addEventListener('change', (event) => {
+    if (event.target.checked) {
+        textoTop.style.backgroundColor = 'transparent';
+        textoInf.style.backgroundColor = 'transparent';
+        textoTop.style.position = 'absolute';
+        textoInf.style.position = 'absolute';
+    }
+    else {
+        textoTop.style.backgroundColor = colorFondoTexto.value;
+        textoInf.style.backgroundColor = colorFondoTexto.value;
+        textoTop.style.position = 'relative';
+        textoInf.style.position = 'relative';
+    }   
+});     
+
+
+// ***********************************************************
+//      CONTORNO 
+// ***********************************************************
+const ninguno = document.getElementById('ninguno');
+const claro = document.getElementById('claro');
+const oscuro = document.getElementById('oscuro');
+
+ninguno.addEventListener('click', () => {
+    textoTop.style.textShadow = '';
+    textoInf.style.textShadow = '';
+});
+claro.addEventListener('click', () => {
+    textoTop.style.textShadow = '-2px -2px 1px #fff, 2px 2px 1px #fff, -2px 2px 1px #fff, 2px -2px 1px #fff';
+    textoInf.style.textShadow = '-2px -2px 1px #fff, 2px 2px 1px #fff, -2px 2px 1px #fff, 2px -2px 1px #fff';
+});
+oscuro.addEventListener('click', () => {
+    textoTop.style.textShadow = '-2px -2px 1px #000, 2px 2px 1px #000, -2px 2px 1px #000, 2px -2px 1px #000';
+    textoInf.style.textShadow = '-2px -2px 1px #000, 2px 2px 1px #000, -2px 2px 1px #000, 2px -2px 1px #000';
+});
+
+// ***********************************************************
+//      ESPACIADO 
+// ***********************************************************
+const padding = document.getElementById('padding-input');
+
+padding.addEventListener('input', () => {
+    textoTop.style.padding = `${padding.value}px`;
+    textoInf.style.padding = `${padding.value}px`;
+});
+
+// ***********************************************************
+//      INTERLINEADO
+// ***********************************************************
+const lineHeight = document.getElementById('heigth-input')
+
+lineHeight.addEventListener('input', () => {
+    textoTop.style.lineHeight = lineHeight.value;
+    textoInf.style.lineHeight =lineHeight.value;
+});
+
+
+
+
